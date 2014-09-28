54c4afed7da22dea7fbe7172452eed5bbf13831b
Alan Ly <me@alanly.ca>
2014-09-28 01:06:48 -0400

    Add our database migration.

---
+++ database/migrations/2014_09_28_045315_create_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateLinksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('links', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('key')->nullable();
+			$table->string('url');
+			$table->integer('count')->unsigned()->default(0);
+			$table->string('creator');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('links');
+	}
+
+}
