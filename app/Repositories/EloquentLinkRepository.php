2e6cea83ec9ca6c4030a3d1752fce3682bc4d58e
Alan Ly <me@alanly.ca>
2014-09-28 00:39:08 -0400

    Add Link repository.

---
+++ app/Repositories/EloquentLinkRepository.php
@@ -0,0 +1,51 @@
+<?php namespace App\Repositories;
+
+use App\Link;
+use App\Repositories\LinkRepository;
+
+class EloquentLinkRepository implements LinkRepository {
+
+	protected $model;
+
+	public function __construct(Link $link)
+	{
+		$this->model = $link;
+	}
+
+	public function delete($model)
+	{
+		return $model->delete();
+	}
+
+	public function getByKey($key)
+	{
+		return $this->model->where('key', $key)->first();
+	}
+
+	public function makeNew(array $attributes = [])
+	{
+		return $this->model->newInstance($attributes);
+	}
+
+	public function save($data)
+	{
+		if ($data instanceOf Link)
+		{
+			return $this->saveModel($data);
+		}
+		elseif (is_array($data))
+		{
+			return $this->saveModel($this->makeNew($data));
+		}
+
+		return false;
+	}
+
+	protected function saveModel(Link $model)
+	{
+		if ($model->getDirty()) return $model->save();
+
+		return $model->touch();
+	}
+	
+}
---
+++ app/Repositories/LinkRepository.php
@@ -0,0 +1,12 @@
+<?php namespace App\Repositories;
+
+use App\Link;
+
+interface LinkRepository {
+
+	public function delete(Link $model);
+	public function getByKey($key);
+	public function makeNew(array $attributes);
+	public function save($data);
+	
+}
