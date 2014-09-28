07e4913ecf7f9dba780a772e6ca6fdc7c9ece9e2
Alan Ly <me@alanly.ca>
2014-09-28 00:37:55 -0400

    Add a Validated model class.

---
+++ app/ValidatedModel.php
@@ -0,0 +1,66 @@
+<?php namespace App;
+
+use App\Exceptions\MissingValidationRuleException;
+use App\Exceptions\MissingValidatorInstanceException;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class ValidatedModel extends Model {
+
+	protected $rules;
+	protected $validator;
+
+	public function save(array $attributes = [])
+	{
+		if (! $this->isValid()) return false;
+
+		return parent::save($attributes);
+	}
+
+	public function isValid()
+	{
+		if (! $this->rules) throw new MissingValidationRuleException();
+
+		if (! $this->validator) throw new MissingValidatorInstanceException();
+
+		return $this->validateInstance();
+	}
+
+	public function getErrors()
+	{
+		if (! $this->validator) throw new MissingValidatorInstanceException();
+
+		return $this->validator->errors();
+	}
+
+	protected function getParsedRules()
+	{
+		$parsedRules = [];
+
+		foreach ($this->rules as $field => $rule)
+		{
+			// Replace '<id>' with existing ID
+			if (str_contains($rule, '<id>'))
+			{
+				$id = $this->exists ? $this->getAttribute($this->primaryKey) : '';
+				$rule = str_replace('<id>', $id, $rule);
+			}
+
+			$parsedRules[$field] = $rule;
+		}
+
+		return $parsedRules;
+	}
+
+	private function validateInstance()
+	{
+		$this->validator = Validator::make(
+			$this->getAttributes(),
+			$this->getParsedRules()
+		);
+
+		return $this->validator->passes();
+	}
+	
+}
